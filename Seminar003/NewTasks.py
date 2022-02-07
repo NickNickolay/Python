0564ff3918dc83a2cbe7708de6340ac18dd06443
Nick <nicknickspb@icloud.com>
2022-02-07 19:57:57 +0300

    Added tasks 33

--- Seminar003/NewTasks.py
+++ Seminar003/NewTasks.py
@@ -33,7 +33,22 @@
 #33. Пользователь задаёт две строки. 
 # Определить количество количество вхождений одной строки в другой.
 
-
+# def numberOfOccurrences(string1, string2): 
+#     text = ' ' 
+#     count = 0 
+#     u = 0 
+#     while u < len(string2): 
+#         for i in string1: 
+#             for j in string2: 
+#                 if i == j: 
+#                     count += 1 
+#                     # text += i 
+#         u +=1 
+#     return (count // u) // u 
+    
+# s1 = input("Введите строку>>> ") 
+# s2 = input("Введите искомую часть>>> ") 
+# print(f'Количество вхождений {s2} в {s1} составляет {numberOfOccurrences(s1, s2)} раз(а)')
 
 
 
